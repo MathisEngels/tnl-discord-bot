929f0fc5140afb791790b350a129ea1bed9c9020
Mathis Engels <mathisengels@yahoo.fr>
2024-12-05 00:29:57 +0100

    fix: use the correct function to push commands to prod

--- src/prod-deploy-slash-commands.ts
+++ src/prod-deploy-slash-commands.ts
@@ -9,9 +9,8 @@ const commandsData = Object.values(commands).map((command) => command.data.toJSO
 (async () => {
   try {
     console.log(`PROD - Started refreshing ${commandsData.length} application (/) commands.`);
-    console.log(commandsData);
 
-    const data = await rest.put(Routes.applicationGuildCommands(config.DISCORD_CLIENT_ID, process.env.GUILD_ID!), { body: commandsData });
+    const data = await rest.put(Routes.applicationCommands(config.DISCORD_CLIENT_ID), { body: commandsData });
     await rest.put(Routes.applicationCommands(config.DISCORD_CLIENT_ID), { body: commandsData });
 
     console.log(`PROD - Successfully reloaded ${(data as string[]).length} application (/) commands.`);
