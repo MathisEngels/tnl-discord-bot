2e8874f7577f1ae181f6a825978a4c34e2bd072b
Mathis Engels <mathisengels@yahoo.fr>
2024-12-13 18:22:27 +0100

    fix: remove duplicate rest request

--- src/dev-deploy-slash-commands.ts
+++ src/dev-deploy-slash-commands.ts
@@ -9,10 +9,8 @@ const commandsData = Object.values(commands).map((command) => command.data.toJSO
 (async () => {
   try {
     console.log(`DEV - Started refreshing ${commandsData.length} application (/) commands.`);
-    console.log(commandsData);
 
     const data = await rest.put(Routes.applicationGuildCommands(config.DISCORD_CLIENT_ID, process.env.GUILD_ID!), { body: commandsData });
-    await rest.put(Routes.applicationGuildCommands(config.DISCORD_CLIENT_ID, "270668625378017280"), { body: commandsData });
 
     console.log(`DEV - Successfully reloaded ${(data as string[]).length} application (/) commands.`);
   } catch (error) {
